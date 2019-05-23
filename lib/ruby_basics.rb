25eed844de878e19c2104d293fd2a2ef57104c95
Fekade Senbeta <fekadesenbeta@gmail.com>
2019-05-23 23:05:43 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,33 @@
 def division(num1, num2)
+  answer = num1 / num2
+  return answer
  
 end
 
 def assign_variable(value)
+  name = value
+  return value
   
 end
 
-def argue
-  
+def argue(argument)
+   argument
 end
 
-def greeting
-
+def greeting(greeting, name)
+  return greeting, name
 end
 
 def return_a_value
-
+  word = "Nice"
+  return word
 end
 
 def last_evaluated_value
-
+  word = "expert"
+  return word
 end
 
-def pizza_party
-
+def pizza_party(topping = "cheese")
+  return topping
 end
